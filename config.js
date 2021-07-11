cf8ebc40e35740cc559f500f62cbfb6138b18d97
timmy0909 <trolimmy09@gmail.com>
2021-07-11 23:04:36 +0200

    lesgo

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+consumer_key: "b72V1g6i4RsJJNZlYFgGvjdNe",
+consumer_secret: "uhXoakwMjfXebzqDl46tnjupWZjD2laIHIAFmdpu3yBhaVTCIm",
+access_token: "1412451372340031495-xa8j1gErlIc5jUBpTkGRND9dwezYfS",
+access_token_secret: "0WIu0DOvhuyAGsf6mUQB8Fh0cF0YCsZSaTlHwdDXizw7d",
+ }
